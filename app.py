57f0ea55a7ce043ffb7052a742de5b38cd06f1c2
Michael Brantley <brantleyrecs@gmail.com>
2024-03-25 20:17:20 -0500

    getting agent setup

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from dotenv import load_dotenv
 
+from griptape.structures import Agent
+
 load_dotenv() # Load the environment variables
+
+# Create The Agent
+agent = Agent()
+
+# Run The Agent
+agent.run("Give me a haiku about skateboarding")
